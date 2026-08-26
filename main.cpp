77f28fa31d16b1402420a41dc1abc39d4c57ee3f
Kenneth Jimenez <kjimenez3802@eagle.fgcu.edu>
2026-08-26 15:26:19 -0400

    Added the recursive function and cleaned up some parts

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
-int recursiveFunctionForward(int arr[],int max);
 int recursiveFunctionBackward(int arr[],int Val);
 int count = 0;
 
+void recFuncBack(int arr[],int Val) {
+    if (Val < 0) return;
+
+    std::cout<<arr[Val]<<" ";
+    recFuncBack(arr,Val-1);
+    ++count;
+}
 
 int main() {
     const int MAX_SIZE = 10;
@@ -26,51 +32,15 @@ int main() {
         }
     }
     std::cout<<"Strongest signal: "<<maxVal<<" at index "<<maxIndex<<std::endl;
+    std::cout<<"Forward path (start to strongest)"<<std::endl;
 
-    //just to see outputs and check
-      for (int i = 0; i < Num; ++i) {
-          std::cout<<signals[i]<<" ";
-      }
-
-    //this is what the code should do
-    for (int i = (Num); i >= 0; --i) {
-        if (i <= maxIndex) {
-            std::cout<<signals[i]<<" ";
-        }
+    for (int i = 0; i < Num; ++i) {
+        std::cout<<signals[i]<<" ";
     }
     std::cout<<std::endl;
-    recursiveFunctionBackward(signals,maxIndex);
-    std::cout<<std::endl;
-    std::cout<<"Cont #: "<<count<<std::endl;
-
+    std::cout<<"Return path (backtracking):"<<std::endl;
+    recFuncBack(signals,Num-1);
+    std::cout<<std::endl<<"Recursive calls made: "<<count<<std::endl;
     return 0;
 }
 
-//recusive function for backtracking
-int recursiveFunctionForward(int arr[],int max)
-{
-    //recursive case
-    //if (n <= 0)
-    if (max >= 0)
-    {
-        recursiveFunctionForward(arr,max-1);
-        std::cout<<arr[max]<<" ";
-        count++;
-    }
-    //base case
-    return count;
-}
-int recursiveFunctionBackward(int arr[],int Val)
-{
-    //base case
-    //if (n <= 0)
-    if (Val == 0)
-    {
-        return count;
-    }
-    //recursive case
-    recursiveFunctionBackward(arr,Val-1);
-    std::cout<<arr[Val]<<" ";
-    count++;
-
-}
