029c8f3ac008e0c1e2357014b2db28271e26159c
RosieThyme <137115930+RosieThyme@users.noreply.github.com>
2026-08-27 07:23:20 -0400

    added in comments

--- main.cpp
+++ main.cpp
@@ -28,42 +28,52 @@ T templateInput()
 }
 int count = 0;
 
-void recFuncBack(int arr[],int Val) {
+void recFuncBack(int arr[],int Val)
+{
+    //base case, checks to see if the Value is negative
     if (Val < 0) return;
-
+    //recursive case that prints out the values and increases the count by one
     std::cout<<arr[Val]<<" ";
     recFuncBack(arr,Val-1);
     ++count;
 }
 
-int main() {
+int main()
+{
+    //variables for input
     const int MAX_SIZE = 10;
     int signals[MAX_SIZE];
     int Num;
     int maxVal, maxIndex;
-
+    //prompt the user for input for the appropriate size of the signal array
     std::cout<<"Enter number of signals that will be input"<<std::endl;
     Num = templateInput<int>();
-
-    for (int i = 0; i < Num; ++i) {
+    //
+    for (int i = 0; i < Num; ++i)
+    {
         std::cout<<"Enter signal: "<<std::endl;
         signals[i] = templateInput<int>();
     }
-
+    //Look for the strongest signal in the Array
     maxVal = signals[0];
-    for (int i = 1; i < Num; ++i) {
-        if (signals[i] > maxVal) {
+    for (int i = 1; i < Num; ++i)
+    {
+        if (signals[i] > maxVal)
+        {
             maxVal = signals[i];
             maxIndex = i;
         }
     }
+    //prints out the strongest signal in the array
     std::cout<<"Strongest signal: "<<maxVal<<" at index "<<maxIndex<<std::endl;
+    //prints out the array from the start of the array to the strongest signal
     std::cout<<"Forward path (start to strongest)"<<std::endl;
-
-    for (int i = 0; i < Num; ++i) {
+    for (int i = 0; i < Num; ++i)
+    {
         std::cout<<signals[i]<<" ";
     }
     std::cout<<std::endl;
+    //prints out the path from the strongest signal to the start
     std::cout<<"Return path (backtracking):"<<std::endl;
     recFuncBack(signals,Num-1);
     std::cout<<std::endl<<"Recursive calls made: "<<count<<std::endl;
