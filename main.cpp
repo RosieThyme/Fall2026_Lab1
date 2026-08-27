58c789c701b039a7b1bb0544e13e5cafd2afa86a
Kenneth Jimenez <kjimenez3802@eagle.fgcu.edu>
2026-08-27 10:34:51 -0400

    wip before merge

--- main.cpp
+++ main.cpp
@@ -1,71 +1,61 @@
 #include <iostream>
 #include <limits>
-#include <utility>
-
 template <typename T>
 T templateInput()
 {
     T tInput;
     bool validInput = false;
+    //as long as the user has not entered a valid input
     while (!validInput)
     {
+        //input validation for the number
         std::cin >> tInput;
+        //if the user does not enter a number, prompt the user to enter the correct information
         if (std::cin.fail())
         {
-            std::cout << "enter in a numeric value:" << std::endl;
+            std::cout<< "enter in a numeric value:"<< std::endl;
             std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
         }
+        //if the user enters the correct information exit the loop
         else
         {
             validInput = true;
         }
     }
+    //return user input
     return tInput;
 }
-
 int count = 0;
 
-void recFuncBack(int arr[], int Val)
+void recFuncBack(int arr[],int Val)
 {
+    //base case, checks to see if the Value is negative
     if (Val < 0) return;
-    std::cout << arr[Val] << " ";
-    recFuncBack(arr, Val - 1);
+    //recursive case that prints out the values and increases the count by one
+    std::cout<<arr[Val]<<" ";
+    recFuncBack(arr,Val-1);
     ++count;
-}
-
-void sortArray(int arr[], int size)
-{
-    for (int i = 0; i < size - 1; ++i)
-    {
-        int minIdx = i;
-        for (int j = i + 1; j < size; ++j)
-        {
-            if (arr[j] < arr[minIdx])
-                minIdx = j;
-        }
-        std::swap(arr[minIdx], arr[i]);
-    }
 }
 
 int main()
 {
+    //variables for input
     const int MAX_SIZE = 10;
     int signals[MAX_SIZE];
     int Num;
     int maxVal, maxIndex;
-
-    std::cout << "Enter number of signals that will be input" << std::endl;
+    //prompt the user for input for the appropriate size of the signal array
+    std::cout<<"Enter number of signals that will be input"<<std::endl;
     Num = templateInput<int>();
-
+    //
     for (int i = 0; i < Num; ++i)
     {
-        std::cout << "Enter signal: " << std::endl;
+        std::cout<<"Enter signal: "<<std::endl;
         signals[i] = templateInput<int>();
     }
-
+    //Look for the strongest signal in the Array
     maxVal = signals[0];
-    maxIndex = 0;
     for (int i = 1; i < Num; ++i)
     {
         if (signals[i] > maxVal)
@@ -74,34 +64,18 @@ int main()
             maxIndex = i;
         }
     }
-    std::cout << "Strongest signal: " << maxVal << " at index " << maxIndex << std::endl;
-
-    std::cout << "Forward path (start to strongest)" << std::endl;
+    //prints out the strongest signal in the array
+    std::cout<<"Strongest signal: "<<maxVal<<" at index "<<maxIndex<<std::endl;
+    //prints out the array from the start of the array to the strongest signal
+    std::cout<<"Forward path (start to strongest)"<<std::endl;
     for (int i = 0; i < Num; ++i)
     {
-        std::cout << signals[i] << " ";
+        std::cout<<signals[i]<<" ";
     }
-    std::cout << std::endl;
-
-    std::cout << "Return path (backtracking):" << std::endl;
-    recFuncBack(signals, Num - 1);
-    std::cout << std::endl << "Recursive calls made: " << count << std::endl;
-
-    int sortedSignals[MAX_SIZE];
-    for (int i = 0; i < Num; ++i)
-        sortedSignals[i] = signals[i];
-
-    sortArray(sortedSignals, Num);
-
-    std::cout << "Sorted ascending: " << std::endl;
-    for (int i = 0; i < Num; ++i)
-        std::cout << sortedSignals[i] << " ";
-    std::cout << std::endl;
-
-    count = 0;
-    std::cout << "Sorted descending (via recursion): " << std::endl;
-    recFuncBack(sortedSignals, Num - 1);
-    std::cout << std::endl << "Recursive calls made: " << count << std::endl;
-
+    std::cout<<std::endl;
+    //prints out the path from the strongest signal to the start
+    std::cout<<"Return path (backtracking):"<<std::endl;
+    recFuncBack(signals,Num-1);
+    std::cout<<std::endl<<"Recursive calls made: "<<count<<std::endl;
     return 0;
 }
